0d89d4d9335d3d0f021f582fe63fa13f30b2c638
Bino Bense <binobense075@gmail.com>
2026-03-09 23:23:06 +0530

    chore: add React element creation using JSX

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: 6 }, [
-    React.createElement(
-      "h1",
-      { id: "header", key: "1" },
-      "Rendered from react",
-    ),
-    React.createElement("h2", { key: 2 }, "Sibiling header"),
-  ]),
-  React.createElement("div", { id: "child2", key: 5 }, [
-    React.createElement("h1", { id: "header", key: 3 }, "2nd child header"),
-    React.createElement("h2", { key: 4 }, "2nd child subheading"),
-  ]),
-]);
-
-console.log(heading);
+const jsxHeading = (
+  <h1 id="header" className="head" tabIndex="1">
+    React using JSX
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
