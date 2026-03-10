8eb7faec52c4bcb8d26f68e3bc59195b2cacd81e
Bino Bense <binobense075@gmail.com>
2026-03-10 07:07:15 +0530

    feat: add first React Component

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//! React Element
 const jsxHeading = (
   <h1 id="header" className="head" tabIndex="1">
     React using JSX
   </h1>
 );
 
+const Title = () => <h1 id="header">Welcome onBoard Crew</h1>;
+
+//! React Component -> Component returns a react element.
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1>This is a react heading component.</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
