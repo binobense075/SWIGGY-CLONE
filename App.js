e64c34ef8f363e4885451bdf4469ff34f092a7e0
Bino Bense <binobense075@gmail.com>
2026-03-25 00:17:07 +0530

    feat: create initial layout structure for header and body sections

--- App.js
+++ App.js
@@ -1,31 +1,93 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//! React Element
-const jsxHeading = (
-  <h1 id="header" className="head" tabIndex="1">
-    React using JSX
-  </h1>
-);
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - SearchBar
+ *  - Restaurnt Container
+ *    - Restaurant cards
+ * Footer
+ *  - Address
+ *  - Copyright
+ *  - Contact
+ *  - Link
+ */
 
-const Title = () => <h1 id="header">Welcome onBoard Crew</h1>;
-
-//! React Component -> Component returns a react element.
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-      {jsxHeading}
-      {console.log("add 100 + 250 = ", 100 + 250)}
-      {console.log("Title", Title)}
-      {console.log("Title", Title())}
-      <Title />
-      <Title></Title>
-      {Title()}
-      <h1>This is a react heading component.</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          alt="logo-image"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="restro-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="restro-logo"
+        alt="restro-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/mocil3npmwpcdcfaxql1"
+      />
+      <h3>Pizza Hut</h3>
+      <h4>Pizzas, Pastas, Italian</h4>
+      <h4>4.4 stars</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app-layout">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
